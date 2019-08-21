9e957dfd4d324392589ad465caba6da22a028e51
Robin Klaus <rmklaus12@gmail.com>
2019-08-21 16:32:51 +1000

    through step 3

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,37 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Flat.destroy_all
+
+puts 'Creating restaurants...'
+flats_attributes = [
+  {
+    name: 'Light & Spacious Garden Flat London',
+    address: '10 Clifton Gardens London W9 1DT',
+    description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+    price_per_night: 75,
+    number_of_guests: 3
+  },
+  {
+    name: 'Large spacious lake home',
+    address: '377 Point of View Dr, Holiday Shores',
+    description: 'Room for the whole family',
+    price_per_night: 275,
+    number_of_guests: 12
+  },
+  {
+    name: "Robin's Nest",
+    address: 'Space 14,Venus Bay Caravan Park',
+    description: 'A quiet retreat away',
+    price_per_night: 15,
+    number_of_guests: 2
+  },
+  {
+    name: 'Conifer Hideaway',
+    address: 'Camano Island, WA',
+    description: 'Gorgeous views of Puget Sound and Mt. Baker',
+    price_per_night: 175,
+    number_of_guests: 4
+  }
+]
+
+Flat.create!(flats_attributes)
+puts 'Finished!'
